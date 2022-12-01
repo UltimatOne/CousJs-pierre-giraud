86e025ea80ab6107def1fd706f01a935610216a9
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2022-12-01 20:55:29 +0100

    Correction de mail

--- test-js/Construction-objet/index.js
+++ test-js/Construction-objet/index.js
@@ -21,9 +21,11 @@ const jeanjacques = new Utilisateur(['Jean-Jacques', 'Goddet'], 29, 'jj.goddet@i
 //Accès (aux valeurs des propriétés en les affichants avec la méthode bonjour() qui envoie une fenetre d'aletre sur la page web
 jeanjacques.bonjour()
 
-//On à le droit de rever... l'age n'est pas bon, corrrection
+//On à le droit de rever... l'age n'est pas bon, correction
 jeanjacques.age = 43;
 
+//une faute s'est glissée dans l'email, correction
+jeanjacques.mail = 'jj.goddet@icloud.com'
 
 /*Affichage des valeurs des propriétés de l'objet créé dans des paragraphes du fichier HTML en manipulation de DOM*/
 
@@ -37,16 +39,20 @@ const s1p2 = document.createElement('p');
 s1p2.id = 's1p2';
 const s1p3 = document.createElement('p');
 s1p3.id = 's1p3';
+const s1p4 = document.createElement('p');
+s1p4.id = 's1p4';
 
 //injection des paragraphes dans la section
-section1.append(s1p1,s1p2,s1p3);
+section1.append(s1p1,s1p2,s1p3,s1p4);
 
 //crétion du contenu a injecter dans les paragraphes
 const contenuS1p1 = document.createTextNode(`Nom complet: ${jeanjacques['nom']}`);
 const contenuS1p2 = document.createTextNode(`Prénom: ${jeanjacques['nom'][0]}`);
 const contenuS1p3 = document.createTextNode(`Age: ${jeanjacques['age']}`);
+const contenuS1p4 = document.createTextNode(`Mail: ${jeanjacques['mail']}`);
 
 //Injection du contenu dans les paragraphes
 s1p1.appendChild(contenuS1p1);
 s1p2.appendChild(contenuS1p2);
 s1p3.appendChild(contenuS1p3);
+s1p4.appendChild(contenuS1p4);
