521b5eeb7e7d7b211079ed461c8be7c4a80ccc34
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2022-12-01 21:44:12 +0100

    Ajout des objets Karine et Maxime avec new Utilisateur et affichage sur la page web

--- test-js/Construction-objet/index.js
+++ test-js/Construction-objet/index.js
@@ -4,8 +4,6 @@
  * @param {age} a proriété age
  * @param {mail} m proriété mail
  */
-
-
 function Utilisateur(n, a, m){
     this.nom = n;
     this.age = a;
@@ -16,16 +14,24 @@ function Utilisateur(n, a, m){
     }
 }
 //création d'un objet jeanjacques avec ses propriété en utilisant le constructeur
-const jeanjacques = new Utilisateur(['Jean-Jacques', 'Goddet'], 29, 'jj.goddet@iclouc.com')
+const jeanjacques = new Utilisateur(['Jean-Jacques', 'Goddet'], 29, 'jj.goddet@iclouc.com');
+
+//création d'un objet karine avec ses propriété en utilisant le constructeur
+const karine = new Utilisateur(['karine', 'Dautel'], 41, '');
+
+//création d'un objet maxime avec ses propriété en utilisant le constructeur
+const maxime = new Utilisateur(['maxime', 'Goddet'], 15, '');
+
+
 
 //Accès (aux valeurs des propriétés en les affichants avec la méthode bonjour() qui envoie une fenetre d'aletre sur la page web
-jeanjacques.bonjour()
+jeanjacques.bonjour();
 
 //On à le droit de rever... l'age n'est pas bon, correction
 jeanjacques.age = 43;
 
 //une faute s'est glissée dans l'email, correction
-jeanjacques.mail = 'jj.goddet@icloud.com'
+jeanjacques.mail = 'jj.goddet@icloud.com';
 
 /*Affichage des valeurs des propriétés de l'objet créé dans des paragraphes du fichier HTML en manipulation de DOM*/
 
@@ -41,18 +47,30 @@ const s1p3 = document.createElement('p');
 s1p3.id = 's1p3';
 const s1p4 = document.createElement('p');
 s1p4.id = 's1p4';
+const s1p5 = document.createElement('p');
+s1p5.id = 's1p5';
+const s1p6 = document.createElement('p');
+s1p6.id = 's1p6';
+const s1p7 = document.createElement('p');
+s1p7.id = 's1p7';
 
 //injection des paragraphes dans la section
-section1.append(s1p1,s1p2,s1p3,s1p4);
+section1.append(s1p1,s1p2,s1p3,s1p4,s1p5,s1p6,s1p7);
 
-//crétion du contenu a injecter dans les paragraphes
+//création du contenu a injecter dans les paragraphes
 const contenuS1p1 = document.createTextNode(`Nom complet: ${jeanjacques['nom']}`);
 const contenuS1p2 = document.createTextNode(`Prénom: ${jeanjacques['nom'][0]}`);
 const contenuS1p3 = document.createTextNode(`Age: ${jeanjacques['age']}`);
 const contenuS1p4 = document.createTextNode(`Mail: ${jeanjacques['mail']}`);
+const contenuS1p5 = document.createTextNode(`Nom complet: ${jeanjacques['nom']}`);
+const contenuS1p6 = document.createTextNode(`Nom complet: ${karine['nom']}`);
+const contenuS1p7 = document.createTextNode(`Nom complet: ${maxime['nom']}`);
 
 //Injection du contenu dans les paragraphes
 s1p1.appendChild(contenuS1p1);
 s1p2.appendChild(contenuS1p2);
 s1p3.appendChild(contenuS1p3);
 s1p4.appendChild(contenuS1p4);
+s1p5.appendChild(contenuS1p5);
+s1p6.appendChild(contenuS1p6);
+s1p7.appendChild(contenuS1p7);
