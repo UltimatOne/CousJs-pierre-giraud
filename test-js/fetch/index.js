f52293dd53a95ec4e2529888915d3b3b7775fa81
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-02-10 12:02:50 +0100

    fetch suite

--- test-js/fetch/index.js
+++ test-js/fetch/index.js
@@ -8,3 +8,19 @@ fetch(`https://dummyjson.com/products`)
 //on traite les cas d'erreur avec catch
 .catch(error => alert("Erreur : " + error));
 
+let promise = fetch(`https://dummyjson.com/products`, {
+    method: "GET", //ou POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "text/plain;charset=UTF-8" //pour un corps de type chaine
+    },
+    body: undefined, //ou string, FormData, Blob, BufferSource, ou URLSearchParams
+    referrer: "about:client", //ou "" (pas de réferanr) ou une url de l'origine
+    referrerPolicy: "no-referrer-when-downgrade", //ou no-referrer, origin, same-origin...
+    mode: "cors", //ou same-origin, no-cors
+    credentials: "same-origin", //ou omit, include
+    cache: "default", //ou no-store, reload, no-cache, force-cache, ou only-if-cached
+    redirect: "follow", //ou manual ou error
+    integrity: "", //ou un hash comme "sha256-abcdef1234567890"
+    keepalive: false, //ou true pour que la requête survive à la page
+    signal: undefined //ou AbortController pour annuler la requête
+});
