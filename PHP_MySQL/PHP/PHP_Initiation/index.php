feb5b3b0738429b1f78805f9b788d137e0cb1335
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-12-28 12:35:08 +0100

    php initiation

---
+++ PHP_MySQL/PHP/PHP_Initiation/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>PHP Initiation</title>
+</head>
+<body>
+    <h1>Titre</h1>
+    <?php 
+        echo "Hello world ! <br><br>";
+
+        echo "Bonjour le monde ! <br><br>";
+
+        echo 'je m\'appelle Jean-Jacques mais tout le monde m\'appelle "JJ" <br><br>';
+
+        echo "je m'appelle Jean-Jacques mais tout le monde m'appelle \"JJ\" <br><br>";
+
+        print 'je m\'appelle Jean-Jacques mais tout le monde m\'appelle "JJ" <br><br>';
+
+        print "je m'appelle Jean-Jacques mais tout le monde m'appelle \"JJ\" <br><br>";
+
+    ?>
+    <p>un paragraphe</p>
+</body>
+</html>
