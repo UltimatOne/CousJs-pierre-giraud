362098279d65b2b64530dcdc5a42a7a0672a4ce5
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-12-29 14:19:34 +0100

    variables

---
+++ PHP_MySQL/PHP/variables/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Variables</title>
+</head>
+<body>
+    <h1>Variables</h1>
+    <?php 
+        $prenom = "Jean-Jacques";
+        $age = 44;
+
+        echo $prenom;
+
+    ?>
+    <p>un paragraphe</p>
+</body>
+</html>
