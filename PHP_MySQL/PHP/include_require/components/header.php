73a7327d5f2d376b9e3f8f96a3b42fe1034fd059
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-03-03 18:07:39 +0100

    MEF + navbar

--- PHP_MySQL/PHP/include_require/components/header.php
+++ PHP_MySQL/PHP/include_require/components/header.php
@@ -4,9 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, , user-scalable=no">
+    <link rel="stylesheet" href="style/style.css">
     <title>Include/require</title>
 </head>
 <body>
     <header>
         <h1>je suis le header, je suis inclus avec include</h1>
+        <?php 
+            include "navbar.php";
+        ?>
     </header>
---
+++ PHP_MySQL/PHP/include_require/components/navbar.php
@@ -0,0 +1,10 @@
+<?php ?>
+
+<nav>
+    <ul>
+        <li><a href="#">Cours Complets</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">A propos</a></li>
+    </ul>
+</nav>
